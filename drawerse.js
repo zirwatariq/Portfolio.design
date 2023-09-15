e784729b0116547871735713eb9332632178bf88
zirwa <ztariq@oswego.edu>
2023-09-14 23:22:44 -0400

    changes

---
+++ drawerse.js
@@ -0,0 +1,36 @@
+//other one 
+  var slideIndex = 2;
+  showSlides(slideIndex);
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+      slideIndex = 2;
+    }
+
+    if (n < 2) {
+      slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 2].style.display = "block";
+    dots[slideIndex - 2].className += " active";
+  }
